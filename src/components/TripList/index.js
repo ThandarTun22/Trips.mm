e58959c09cba02706f5287450e992f98c25a476d
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-05-06 09:28:07 +0630

    useCallback (cache version of a function )

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -1,34 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import './index.css'
 
 export default function Index() {
 
   let [trips, setTrips] = useState([]);
   let [url, setUrl] = useState('http://localhost:3001/trips');
 
-  useEffect(()=>{
+  let fetchTrips = useCallback(()=> {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-      setTrips(data);
-      console.log(trips)
+      setTrips(data);   
     })
-  }, [url])
+  },[url])
 
+  useEffect(()=>{
+    fetchTrips()
+  }, [fetchTrips])
+
+  console.log(trips)
   return (
-    <div>
-        <h1>Ready to go ?</h1>
+    <div className='container'>
+        <div className="flex-container">
+          <h1>Ready to go ?</h1>
 
-        <button onClick={()=>setUrl('http://localhost:3001/trips')}>all</button>
-        <button onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')}>filter in myanmar</button>
+          <div>
+            <button onClick={()=>setUrl('http://localhost:3001/trips')}>all</button>
+            <button onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')}>filter in myanmar</button>
+          </div>
 
-        <ul>{trips.map(trip => (
-          <li key={trip.id}> 
-          <h3>{trip.name}</h3>
-          <p>price - {trip.price} mmk</p>
-      </li>
-        ))}
-            
-        </ul>
+          <ul className='trips-list'>{trips.map(trip => (
+            <li key={trip.id} className='trip'> 
+            <h3>{trip.name}</h3>
+            <p>price - {trip.price} mmk</p>
+          </li>
+          ))}
+              
+          </ul>
+        </div>
     </div>
   )
 }
